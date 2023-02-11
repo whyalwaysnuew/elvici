f173888ab18b5d29e1d0638f627f8d616e466dc9
whyalwaysme <coworasavanila@gmail.com>
2023-02-11 21:26:11 +0700

    init user component

--- app/Mail/User/AccountRegister.php
+++ app/Mail/User/AccountRegister.php
@@ -43,7 +43,7 @@ class AccountRegister extends Mailable
     public function content()
     {
         return new Content(
-            view: 'view.mail.register',
+            view: 'emails.user.register',
         );
     }
 
